38dfbaba2889d4368bf798c083e6d0bef0d303de
Paul Jobson <pjobson@gmail.com>
2015-02-26 15:33:43 -0500

    big update, including buffering message.

--- htmlTemplate/assets/controller.js
+++ htmlTemplate/assets/controller.js
@@ -1,10 +1,24 @@
+var percentTxt = {
+	'0': ['   _|   ',' _|  _| ',' _|  _| ',' _|  _| ','   _|   '],
+	'1': ['   _| ',' _|_| ','   _| ','   _| ','   _| '],
+	'2': ['   _|_|   ',' _|    _| ','     _|   ','   _|     ',' _|_|_|_| '],
+	'3': [' _|_|_|   ','       _| ','   _|_|   ','       _| ',' _|_|_|   '],
+	'4': [' _|  _|   ',' _|  _|   ',' _|_|_|_| ','     _|   ','     _|   '],
+	'5': [' _|_|_|_| ',' _|       ',' _|_|_|   ','       _| ',' _|_|_|   '],
+	'6': ['   _|_|_| ',' _|       ',' _|_|_|   ',' _|    _| ','   _|_|   '],
+	'7': [' _|_|_|_|_| ','         _| ','       _|   ','     _|     ','   _|       '],
+	'8': ['   _|_|   ',' _|    _| ','   _|_|   ',' _|    _| ','   _|_|   '],
+	'9': ['   _|_|   ',' _|    _| ','   _|_|_| ','       _| ',' _|_|_|   '],
+	'%': [' _|_|    _| ',' _|_|  _|   ','     _|     ','   _|  _|_| ',' _|    _|_| '],
+	'/': ['         _| ','       _|   ','     _|     ','   _|       ',' _|         ']
+};
 
 var buffering = [
-	'..... ____  _   _ _____ _____ _____ ____  ___ _   _  ____ .....',
-	'.....| __ )| | | |  ___|  ___| ____|  _ \\|_ _| \\ | |/ ___|.....',
-	'.....|  _ \\| | | | |_  | |_  |  _| | |_) || ||  \\| | |  _ .....',
-	'.....| |_) | |_| |  _| |  _| | |___|  _ < | || |\\  | |_| |.....',
-	'.....|____/ \\___/|_|   |_|   |_____|_| \\_\\___|_| \\_|\\____|.....'
+	' ____  _   _ _____ _____ _____ ____  ___ _   _  ____ ',
+	'| __ )| | | |  ___|  ___| ____|  _ \\|_ _| \\ | |/ ___|',
+	'|  _ \\| | | | |_  | |_  |  _| | |_) || ||  \\| | |  _ ',
+	'| |_) | |_| |  _| |  _| | |___|  _ < | || |\\  | |_| |',
+	'|____/ \\___/|_|   |_|   |_____|_| \\_\\___|_| \\_|\\____|'
 ];
 var clickPlay = [
 	"  ____ _     ___ ____ _  __  ____  _        _ __   __",
@@ -16,10 +30,17 @@ var clickPlay = [
 
 var framePrefetchCount = 1;
 var msgChngd = false;
+var playClicked = false;
+
 var prefetcher = function() {
-	if (framePrefetchCount >= Math.floor(counter.last/6) && msgChngd===false) {
-		msgChngd = true;
-		$('#movie').html(clickPlay.join('\n'));
+	if (playClicked === false) {
+		var percent = Math.floor((framePrefetchCount/counter.last)*100);
+		bufferingMsgUpdate(percent);
+
+		if (percent===100 && msgChngd===false) {
+			msgChngd = true;
+			bufferingMsgUpdate('play');
+		}
 	}
 
 	$.ajax({
@@ -30,6 +51,12 @@ var prefetcher = function() {
 		}
 	});
 };
+
+var consolelog = function(msg) {
+	if (console) {
+		console.log(msg);
+	}
+}
 
 
 var framePadder = function(f) {
@@ -43,7 +70,7 @@ var framePadder = function(f) {
 
 var showFrame = function(frame) {
 	// These should be cached by now, so this will load basically instantly.
-	console.log('frame:'+ frame);
+	consolelog('showing frame:'+ frame);
 	$.ajax({
 		url: 'txt/img.'+ framePadder(frame) +'.txt',
 		success: function(res) {
@@ -56,22 +83,46 @@ var intervalSpooler = function() {
 	showFrame(counter.frame++);
 };
 
-var player = new MediaElementPlayer('#player',{
-	plugins: ['flash','silverlight'],
-	flashName: 'flashmediaelement.swf',
-	silverlightName: 'silverlightmediaelement.xap',
-	success: function (mediaElement, domObject) {
-		prefetcher();
-		$('#movie').html(buffering.join('\n'));
-		$('#player').bind('play', function(e) {
-			window.spool = window.setInterval(intervalSpooler,counter.timeout);
-		});
-		$('#player').bind('pause', function(e) {
-			window.clearInterval(window.spool);
-		});
-		$('#player').bind('timeupdate', function(e) {
-			// This tries to sync the current frame with the music
-			counter.frame = Math.floor(e.currentTarget.currentTime * counter.fps);
-		});
+var drawPercent = function(num) {
+	num = (new String(num+'/100%')).split('');
+	var txt = ['','','','',''];
+	for (var i=0;i<num.length;i++) {
+		for (var j=0;j<txt.length;j++) {
+			txt[j] += percentTxt[num[i]][j];
+		}
 	}
-});
+	return txt.join('\n');
+};
+
+var bufferingMsgUpdate = function(perc) {
+	if (perc === 'play') {
+		$('#movie').html(clickPlay.join('\n'));
+		$('#player').show();
+	} else {
+		var txt  = buffering.join('\n') +'\n\n\n'+ drawPercent(perc);
+		$('#movie').html(txt);
+	}
+};
+
+var init = function() {
+	$('#player').hide();
+	bufferingMsgUpdate(0);
+	prefetcher();
+
+	$('#player').bind('play', function(e) {
+		playClicked = true;
+		window.spool = window.setInterval(intervalSpooler,counter.timeout);
+		consolelog('play clicked');
+	});
+	$('#player').bind('pause', function(e) {
+		window.clearInterval(window.spool);
+		consolelog('pause clicked');
+	});
+	$('#player').bind('timeupdate', function(e) {
+		// This tries to sync the current frame with the music
+		counter.frame = Math.floor(e.currentTarget.currentTime * counter.fps);
+		consolelog('Syncing frame to: '+ counter.frame);
+	});
+};
+
+init();
